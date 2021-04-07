dba1d8669e54042c0d92236f157358c2b98c98f3
Delta9k8b <keaton.brewster@gmail.com>
2021-04-07 13:54:58 -0500

    trying to get db working online with heroku. Not sure if im close yet, but I am trying

--- config/index.js
+++ config/index.js
@@ -6,18 +6,54 @@ const {
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
-const uri = "mongodb+srv://FitnessTracker:FitnessTrackerDBKPB@cluster0-shard-00-01.baqrb.mongodb.net:27017/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-async function main() {
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.baqrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect = async () => {
     try {
         await client.connect();
     } catch {
         error => {
             console.error(`client connection error /config/::17 ==> ${error}`);
         };
-    } finally {
-        await client.close();
     }
 }
 
-module.exports = main;
+disconnect = async () => {
+    try {
+        await client.close();
+    } catch {
+        error => {
+            console.error(`disconnect error /config/::27 ==> ${error}`);
+        }
+    }
+}
+
+
+show_dbs = async (client) => {
+    try {
+        await client.connect()
+        databasesList = await client.db().admin().listDatabases();
+
+        console.log("Databases:");
+        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    } catch {
+        error => {
+            throw new Error(error);
+        }
+    } finally {
+        await client.close();
+    }
+
+}
+
+module.exports = remoteDB = {
+    client: client,
+    connect: connect,
+    disconnect: disconnect,
+    show_dbs: show_dbs,
+};
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dbConnect = require('./config');
+const {
+    client,
+    connect,
+    disconnect,
+    show_dbs
+} = require('./config');
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+
 
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 app.use(express.static('public'));
+
+//! MAY NOT WANT TO USE THESE IN THE FINAL APP, BUT FOR NOW I AM JUST TRYING TO GET THE REMOTE DB WORKING
+process.env.MONGODB_PASS = 'FitnessTrackerDBKPB';
+process.env.MONGODB_USER = 'FitnessTracker'
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
@@ -19,7 +30,7 @@ require('./controllers/api')(app);
 require('./controllers/html')(app);
 
 try {
-    dbConnect();
+    show_dbs(client);
     app.listen(PORT, async () => {
         console.log(`Listening on port ${PORT}.`);
     });
