6272851d77df98759cd3af458f95ddc301d02e25
Delta9k8b <keaton.brewster@gmail.com>
2021-04-08 06:22:15 -0500

    removing unnecessary package.

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 const compression = require('compression');
 const express = require('express');
 const mongoose = require('mongoose');
-const {
-    MongoClient
-} = require('mongodb');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -16,13 +13,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static('public'));
 
-const URI = process.env.MONGODB_URI;
-const client = new MongoClient(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.connect(URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true
@@ -32,7 +23,6 @@ require('./controllers/api')(app);
 require('./controllers/html')(app);
 
 try {
-    client.connect();
     app.listen(PORT, async () => {
         console.log(`Listening on port ${PORT}.`);
     });
