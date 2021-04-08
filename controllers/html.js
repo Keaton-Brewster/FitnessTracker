2c341bfaf9751ecd99af533c7605723feaa35651
Delta9k8b <keaton.brewster@gmail.com>
2021-04-07 21:21:39 -0500

    integrated minimized versions of all client side scripts and css files

--- controllers/html.js
+++ controllers/html.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/', (request, response) => {
-        response.sendFile(path.resolve('public/assets/index.html'));
+        response.sendFile(path.resolve('public/assets/html/index.html'));
     });
 
     app.get('/exercise', (request, response) => {
-        response.sendFile(path.resolve('public/assets/exercise.html'));
+        response.sendFile(path.resolve('public/assets/html/exercise.html'));
     });
 
     app.get('/stats', (request, response) => {
-        response.sendFile(path.resolve('public/assets/stats.html'))
+        response.sendFile(path.resolve('public/assets/html/stats.html'))
     })
 }
